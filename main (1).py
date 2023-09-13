99ce26380ec53c81c92c2ae6c3084493c076518a
005sk005 <144887396+005sk005@users.noreply.github.com>
2023-09-13 14:44:47 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+# Leap year
+
+def tsLeapYear(year):
+  if(year % 4==0 and  year % 100 !=0 ) or year % 400 == 0:
+     return True 
+  else:
+    return False
+
+year =2020
+
+if tsLeapYear(year):
+    print("{} is  a leap year.",format(year))
+else:
+    print("{} is not a leap year.",format(year))
